ef70e7d6872291165577b5d7759e87fec10f4ab2
ZS\sc31079 <shaunak.chakraborty@zs.com>
2023-05-06 14:32:10 +0530

    added endpoint with query and path parameter

--- main.py
+++ main.py
@@ -152,6 +152,24 @@ async def read_own_items(current_user: User = Depends(get_current_active_user)):
 
 
 
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+
+
 # pwd_hash = get_password_hash("shaunak1234")
 # print(pwd_hash)
 
