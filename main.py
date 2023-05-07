1d10e2c7a9ab96f0f89b040a2d36f67d830d71be
ZS\sc31079 <shaunak.chakraborty@zs.com>
2023-05-07 23:12:13 +0530

    added post endpoint with Basic auth for XML testing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import Depends, FastAPI, HTTPException, status, Response, Request
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm 
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+from typing import Annotated
 from pydantic import BaseModel
 from datetime import timedelta, datetime
 from jose import JWTError, jwt 
@@ -168,15 +171,48 @@ async def read_user_item(
     return item
 
 
+
+
+
+
+security = HTTPBasic()
+
+
+def get_current_username(
+    credentials: Annotated[HTTPBasicCredentials, Depends(security)]
+):
+    current_username_bytes = credentials.username.encode("utf8")
+    correct_username_bytes = b"stanleyjobson"
+    is_correct_username = secrets.compare_digest(
+        current_username_bytes, correct_username_bytes
+    )
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = b"swordfish"
+    is_correct_password = secrets.compare_digest(
+        current_password_bytes, correct_password_bytes
+    )
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.get("/users/me")
+def read_current_user(username: Annotated[str, Depends(get_current_username)]):
+    return {"username": username}
+
+
 @app.post("/submit")
-async def submit(request: Request):
+async def submit(request: Request, username: Annotated[str, Depends(get_current_username)]):
     content_type = request.headers['Content-Type']
     if content_type == 'application/xml':
         body = await request.body()
         return Response(content=body, media_type="application/xml")
     else:
         raise HTTPException(status_code=400, detail=f'Content type {content_type} not supported')
-
 
 
 
