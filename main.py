3fc73b4fc9e264df7771a8bd548fc3e335e4b7a1
ZS\sc31079 <shaunak.chakraborty@zs.com>
2023-05-09 16:47:29 +0530

    added scope in access_token data

--- main.py
+++ main.py
@@ -133,6 +133,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     print(form_data)
     print(form_data.scopes)
 
+    scope = " ".join(form_data.scopes) if form_data.scopes else "*"
+
     user = authenticate_user(db, form_data.username, form_data.password)
     print(f"User : {user}")
 
@@ -140,7 +142,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect username/password", headers = {"WWW-Authenticate":"Bearer"})
     
     access_token_expires = timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data = {"sub" : user.username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data = {"sub" : user.username, "scope": scope}, expires_delta=access_token_expires)
 
     print(access_token)
 
